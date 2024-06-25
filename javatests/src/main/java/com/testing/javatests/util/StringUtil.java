d894cc79e5d62fb7ce7d5613dc66aff156834499
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-06-25 13:28:54 -0400

    test with Junit

--- javatests/src/main/java/com/testing/javatests/util/StringUtil.java
+++ javatests/src/main/java/com/testing/javatests/util/StringUtil.java
@@ -1,4 +1,19 @@
 package com.testing.javatests.util;
 
 public class StringUtil {
+
+    public static  String repeat(String s, int times) {
+
+        if (times < 0) {
+            throw new IllegalArgumentException("negative times not allowed");
+        }
+
+        String result = "";
+
+        for (int i = 0; i < times; i++) {
+            result += s;
+        }
+        return result;
+    }
+
 }
---
+++ javatests/src/test/java/com/testing/javatests/util/StringUtilTest.java
@@ -0,0 +1,26 @@
+package com.testing.javatests.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StringUtilTest {
+    @Test
+    public void repeact_string_one() {
+        Assert.assertEquals(StringUtil.repeat("hola", 1), "hola");
+    }
+
+    @Test
+    public void repeat_string_multiple_times() {
+        Assert.assertEquals(StringUtil.repeat("hola", 3), "holaholahola");
+    }
+
+    @Test
+    public void repeat_string_zero_times() {
+        Assert.assertEquals(StringUtil.repeat("hola", 0), "");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void repeat_string_negative_times() {
+        StringUtil.repeat("hola", -1);
+    }
+}
