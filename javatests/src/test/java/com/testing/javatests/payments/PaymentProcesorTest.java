9083cdbd29a856462657337b0841fa9d59642d52
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-06-28 11:42:19 -0400

    refactor

--- javatests/src/test/java/com/testing/javatests/payments/PaymentProcesorTest.java
+++ javatests/src/test/java/com/testing/javatests/payments/PaymentProcesorTest.java
@@ -1,5 +1,6 @@
 package com.testing.javatests.payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -7,25 +8,34 @@ import static org.junit.Assert.*;
 
 public class PaymentProcesorTest {
 
+    private PaymentGateway paymentGateway;
+    private PaymentProcesor paymentProcesor;
+
+    @Before
+    public void setup() {
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcesor = new PaymentProcesor(paymentGateway);
+    }
+
     @Test
     public void payment_is_correct() {
 
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
+
         Mockito.when(paymentGateway.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
-        PaymentProcesor paymentProcesor = new PaymentProcesor(paymentGateway);
+
 
         boolean result = paymentProcesor.makePayment(1000);
+
         assertTrue(result);
     }
 
     @Test
     public void payment_is_wrong() {
 
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
-        PaymentProcesor paymentProcesor = new PaymentProcesor(paymentGateway);
 
         boolean result = paymentProcesor.makePayment(1000);
+
         assertFalse(result);
     }
 }
